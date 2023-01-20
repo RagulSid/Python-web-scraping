5bd03fd54b170f0ee5b7f2c8eb7aba703559edde
RagulSid <ragulsiddharth1@gmail.com>
2023-01-20 20:52:00 +0530

    Headless chrome test - background running  code added

---
+++ HeadlessChrome.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from time import sleep
+
+class MyBot:
+    def __init__(self) :
+        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
+
+        self.options = webdriver.ChromeOptions()
+        self.options.headless = True
+        self.options.add_argument(f'user-agent={user_agent}')
+        self.options.add_argument("--window-size=1920,1080")
+        self.options.add_argument('--ignore-certificate-errors')
+        self.options.add_argument('--allow-running-insecure-content')
+        self.options.add_argument("--disable-extensions")
+        self.options.add_argument("--proxy-server='direct://'")
+        self.options.add_argument("--proxy-bypass-list=*")
+        self.options.add_argument("--start-maximized")
+        self.options.add_argument('--disable-gpu')
+        self.options.add_argument('--disable-dev-shm-usage')
+        self.options.add_argument('--no-sandbox')
+        self.driver = webdriver.Chrome(executable_path="chromedriver.exe", options=self.options)
+
+        self.driver.get("https://google.com")
+        self.driver.get_screenshot_as_file("screenshot.png")
+        print(self.driver.title)
+
+MyBot()
+
+
+
